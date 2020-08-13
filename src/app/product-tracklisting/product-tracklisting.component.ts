bf0bba501ee5a0f1b200ace4111ac7e9929d290a
Beke Dube <beke.dube@cir2.com>
2020-08-13 18:43:29 -0500

    productlisting

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Album } from '../album';
 import { ProductService } from '../product.service';
+import { Album } from '../album';
+
 
 @Component({
   selector: 'app-product-tracklisting',
@@ -15,7 +16,8 @@ export class ProductTracklistingComponent implements OnInit {
 
 
   ngOnInit() {
-      this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response =>
+      this.albumInfo = response);
   }
 
 }
